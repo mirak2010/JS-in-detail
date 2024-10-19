dd2b75d73801ce8f8275422bc456749e3ea25e85
mirak2010 <mirakhmad0101@yahoo.com>
2024-10-19 10:39:35 +0500

    Done

---
+++ errorhandlinghw.js
@@ -0,0 +1,21 @@
+try{
+    {
+        var x=a;
+        var b=d;
+    }
+}
+
+catch(err){
+
+    document.write(err.message);
+
+}
+document.write("<br>")
+try{
+    var x=d;
+}
+catch (err) {
+
+    document.write(err.message);
+
+}
